0dd40d366c2539372347048a8024f1f4380e8a26
Web-Who <webwho.br@gmail.com>
2025-09-21 19:43:25 -0300

    feat: Define initial routes for the application with Dashboard component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,18 @@
+import Dashboard from '@/views/dashboard.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Dashboard',
+      component: Dashboard,
+    },
+  ],
+
+  //todo: redirecinar rotas inexistentes
+
 })
 
 export default router
